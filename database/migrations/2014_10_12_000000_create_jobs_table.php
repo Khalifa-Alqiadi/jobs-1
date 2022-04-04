abde86a793958711cc3bc23e6a4dc38969f40748
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-04-05 02:33:48 +0300

    commit init

--- database/migrations/2014_10_12_000000_create_jobs_table.php
+++ database/migrations/2014_10_12_000000_create_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateJobsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('description');
+            $table->string('address');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('is_active')->default(1);
+            $table->string('image');
         });
     }
 
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jobs');
     }
 }
